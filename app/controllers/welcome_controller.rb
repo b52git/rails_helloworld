61db02108efe1dbc95dc04cdd588443fe0f1d3e8
Edward Rich <edwardwrich@gmail.com>
2015-05-05 10:35:19 -0400

    test rails app

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,11 @@
 class WelcomeController < ApplicationController
+
+  def index
+  @favorite_language = sample_languages
+  end
+
+  def sample_languages
+    ['ruby', 'swift', 'javascript', 'c#'].sample
+  end
+
 end
